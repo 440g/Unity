b9d1f98e9dfe00c390cf7c5112e396ca9c2414c3
MINSEO KIM <meanwestk@gmail.com>
2025-03-20 09:17:47 +0900

    update d6

--- Assets/D06_UI/D06_UIGameObjects.cs
+++ Assets/D06_UI/D06_UIGameObjects.cs
@@ -1,9 +1,11 @@
 using System.Collections; 
 using System.Collections.Generic; 
 using UnityEngine; 
+using UnityEngine.SceneManagement; 
 public class D06_UIGameObjects : MonoBehaviour 
 { 
     public void OnClick_Print(GameObject Target){ 
         print(Target.name); 
+        SceneManager.LoadScene(0); //문자열로 씬 이름 넣어도 작동함 
     } 
 } 
